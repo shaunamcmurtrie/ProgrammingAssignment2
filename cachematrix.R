ab820a6a0c448d8533e166a7e9c0bf56899977d6
shaunamcmurtrie <shauna.mcmurtrie@rittmanmead.com>
2014-07-27 22:51:32 +0100

    Final Assignment Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   invMatrix <- NULL
+   set <- function(y) {
+      x <<- y
+      invMatrix <<- NULL
+   }
+   get <- function() x
+   setInvMatrix <- function(im){
+     invMatrix <<- im
+     invMatrix <- im
+   } 
+   getInvMatrix <- function(){
+     invMatrix
+   }
+   list(set = set, get = get,
+    setInvMatrix = setInvMatrix,
+    getInvMatrix = getInvMatrix)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  iMtrx <- x$getInvMatrix()
+  if(!is.null(iMtrx)){
+    message("getting cached data")
+    
+    return(iMtrx)
+  }
+  data <- x$get()
+  iMtrx <- solve(data)
+  x$setInvMatrix(iMtrx)
+  iMtrx
 }
